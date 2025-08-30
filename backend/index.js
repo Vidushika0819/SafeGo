610690cfc9deef5f021cf5136b0b12b1eec911b6
Vidushika <withanagevidhu@gmail.com>
2025-08-30 11:38:55 +0530

    add index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
-require("dotenv").config();
- 
+const mongoose = require("mongoose");
+const router = require("./Route/userRouter"); //insert route
+
 const app = express();
+const cors = require("cors");
+
+const PORT = 5000;
+
+//Middleware
 app.use(express.json());
- 
-// Routes
-app.get("/", (req, res) => {
-  res.send("API is working... 🚀");
-});
- 
-// Server listen
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(cors());
+app.use("/users",router);
+app.use("/files",express.static("files"));
+
+mongoose.connect("mongodb+srv://vidushika:BfgwUDnfQIe71WHG@cluster0.hxe46hj.mongodb.net/")
+.then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(5000, () => {
+        console.log("🚀 Server is running on port 5000");
+    });
+})
+.catch((err) => console.log("❌ MongoDB Connection Error:", err));
